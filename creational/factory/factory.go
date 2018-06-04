1f162ffb590945456354bd63491eb49e969f3c52
Enze Chi <enze.chi@gmail.com>
2018-06-04 15:50:14 +1000

    Add factory pattern

---
+++ creational/factory/factory.go
@@ -0,0 +1,38 @@
+package factory
+
+import (
+	"fmt"
+)
+
+const (
+	Cash      = 1
+	DebitCard = 2
+)
+
+type PaymentMethod interface {
+	Pay(amount float32) string
+}
+
+func GetPaymentMethod(m int) (PaymentMethod, error) {
+	switch m {
+	case Cash:
+		return new(CashPM), nil
+
+	case DebitCard:
+		return new(DebitCardPM), nil
+
+	default:
+		return nil, fmt.Errorf("payment method %d not recognized\n", m)
+	}
+}
+
+type CashPM struct{}
+type DebitCardPM struct{}
+
+func (c *CashPM) Pay(amount float32) string {
+	return fmt.Sprintf("%0.2f paid using cash\n", amount)
+}
+
+func (c *DebitCardPM) Pay(amount float32) string {
+	return fmt.Sprintf("%#0.2f paid using debit card\n", amount)
+}
