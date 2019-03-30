ca24fc7134b5ec9ed7d548a2929cddbae88b9fe7
Enze Chi <enze.chi@gmail.com>
2019-03-30 20:56:58 +1100

    Add BusBuilder and test

--- creational-patterns/builder/builder.go
+++ creational-patterns/builder/builder.go
@@ -70,3 +70,26 @@ func (b *BikeBuilder) SetStructure() BuildProcess {
 func (b *BikeBuilder) GetVehicle() VehicleProduct {
 	return b.v
 }
+
+type BusBuilder struct {
+	v VehicleProduct
+}
+
+func (b *BusBuilder) SetWheels() BuildProcess {
+	b.v.Wheels = 8
+	return b
+}
+
+func (b *BusBuilder) SetSeats() BuildProcess {
+	b.v.Seats = 30
+	return b
+}
+
+func (b *BusBuilder) SetStructure() BuildProcess {
+	b.v.Structure = "Bus"
+	return b
+}
+
+func (b *BusBuilder) GetVehicle() VehicleProduct {
+	return b.v
+}
--- creational-patterns/builder/builder_test.go
+++ creational-patterns/builder/builder_test.go
@@ -30,7 +30,6 @@ func TestBuilderPatter(t *testing.T) {
 	manufactoringComplex.Construct()
 
 	motorbike := bikeBuiler.GetVehicle()
-	motorbike.Seats = 1
 
 	if motorbike.Wheels != 2 {
 		t.Errorf("wheels on motobike must be 2 and they where %d\n", motorbike.Wheels)
@@ -38,5 +37,28 @@ func TestBuilderPatter(t *testing.T) {
 
 	if motorbike.Structure != "Motorbike" {
 		t.Errorf("structure on a motorbike must be 'Motorbike' and was %s\n", motorbike.Structure)
+	}
+
+	if motorbike.Seats != 2 {
+		t.Errorf("seats on a motorbike must be 2 and they were %d\n", motorbike.Seats)
+	}
+
+	busBuilder := &BusBuilder{}
+
+	manufactoringComplex.SetBuilder(busBuilder)
+	manufactoringComplex.Construct()
+
+	bus := busBuilder.GetVehicle()
+
+	if bus.Wheels != 8 {
+		t.Errorf("wheels on bus must be 8 and they were %d\n", bus.Wheels)
+	}
+
+	if bus.Structure != "Bus" {
+		t.Errorf("structure on a bus must be 'Bus' and was %s\n", bus.Structure)
+	}
+
+	if bus.Seats != 30 {
+		t.Errorf("seats on a bus must be 30 and they were %d\n", bus.Seats)
 	}
 }
